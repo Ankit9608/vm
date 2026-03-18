3bb2cfd93ee10c7ab3c8f304d7737b1c32f6df4a
ankit <ankitpatilavp@gmail.com>
2026-03-18 23:01:57 +0530

    added redeem.py

---
+++ redeem.py
@@ -0,0 +1,72 @@
+import os
+from web3 import Web3
+from py_builder_relayer_client.client import RelayClient
+
+from py_builder_relayer_client.models import OperationType, SafeTransaction
+
+from py_builder_signing_sdk.config import BuilderConfig
+from py_builder_signing_sdk.sdk_types import BuilderApiKeyCreds
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Setup client
+builder_config = BuilderConfig(
+    local_builder_creds=BuilderApiKeyCreds(
+        key=os.getenv("POLY_BUILDER_API_KEY"),
+        secret=os.getenv("POLY_BUILDER_SECRET"),
+        passphrase=os.getenv("POLY_BUILDER_PASSPHRASE"),
+    )
+)
+
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+
+
+client = RelayClient(
+    "https://relayer-v2.polymarket.com", 137, PRIVATE_KEY, builder_config
+)
+CTF = "0x4D97DCd97eC945f40cF65F87097ACe5EA0476045"
+parent_collection_id = b"\x00" * 32
+collateral_token = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
+condition_id = "0x498e8f5cf3135bb6c94ea30793f01fbadae005567a7bea47c20f1ce1ff8beb22"
+index_sets = [1, 2]
+
+
+# class Transaction:
+#     def __init__(self, to, data, value=0):
+#         self.to = to
+#         self.data = data
+#         self.value = value
+
+
+redeem_tx = SafeTransaction(
+    to=CTF,
+    operation=OperationType.Call,
+    data=Web3()
+    .eth.contract(
+        address=CTF,
+        abi=[
+            {
+                "name": "redeemPositions",
+                "type": "function",
+                "inputs": [
+                    {"name": "collateralToken", "type": "address"},
+                    {"name": "parentCollectionId", "type": "bytes32"},
+                    {"name": "conditionId", "type": "bytes32"},
+                    {"name": "indexSets", "type": "uint256[]"},
+                ],
+                "outputs": [],
+            }
+        ],
+    )
+    .encode_abi(
+        abi_element_identifier="redeemPositions",
+        args=[collateral_token, parent_collection_id, condition_id, index_sets],
+    ),
+    value="0",
+)
+
+response = client.execute([redeem_tx], "Redeem positions")
+response.wait()
+print(type(response))
+print(response)
